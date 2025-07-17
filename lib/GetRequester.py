dd6db160f2159d2af50f08ba2c769e95e32b10a2
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-07-16 23:09:22 -0400

    Adds in code to fetch api data via request.get for get_response_body & load_json

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -9,6 +9,10 @@ class GetRequester:
 
     def get_response_body(self):
         # Make a GET request to the URL and return raw content
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
         # Make a GET request and parse the JSON into Python data
+        response = requests.get(self.url)
+        return response.json()

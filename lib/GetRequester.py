f571fefedc582d47c33369923991c5522bda9d6e
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-07-16 23:06:50 -0400

    Starts initial pseudocode

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,14 @@
 import requests
 import json
 
+
 class GetRequester:
 
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        # Make a GET request to the URL and return raw content
 
     def load_json(self):
-        pass
+        # Make a GET request and parse the JSON into Python data
